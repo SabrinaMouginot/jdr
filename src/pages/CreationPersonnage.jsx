962c74341c4590985df5a83824850daf17f92c38
SabrinaMouginot <s.mouginot@yahoo.fr>
2025-01-08 00:21:43 +0100

    creation des 3 pages Home - CreationPersonnage - Jeu

---
+++ src/pages/CreationPersonnage.jsx
@@ -0,0 +1,7 @@
+function CreationPersonnage() {
+    return (
+        <div></div>
+    );
+  }
+  
+  export default CreationPersonnage;
---
+++ src/pages/Jeu.jsx
@@ -0,0 +1,7 @@
+function Jeu() {
+    return (
+        <div></div>
+    );
+  }
+  
+  export default Jeu;
